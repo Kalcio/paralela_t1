9d754361144ee4eec7efdf21721c08d3f68e25e8
Kalcio <mmunozm@utem.cl>
2022-09-02 18:16:42 -0400

    Creación vector coef

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@
  */
 int main(int argc, char **argv) {
   int exitCode;
+  std::vector<double> coeff;
 
   /// Si no hay argumentos suficientes mostramos un texto por defecto.
   if (argc != 3) {
@@ -26,15 +27,36 @@ int main(int argc, char **argv) {
     const double valor = std::atof(argv[2]);
     std::cout << std::endl << "polinomio (cmd): " << polinomio << std::endl;
     std::cout << "Valor: " << valor << std::endl;
-
-    std::cout << "polinomio (convertido): ";
+    // std::cout << "polinomio (convertido): ";
     std::vector<Monomio> monomios = Utils::convertir(polinomio);
     std::vector<Monomio>::size_type i = 0;
+    for(i=0; i < monomios.size();i++){
+        Monomio monomio = monomios[i];
+        // coeff[i] = monomio.GetCoeficiente();
+        double aux = monomio.GetGrado();
+        coeff.push_back(aux);
+        // std::cout << "Coef" << i << ": " << monomio.GetCoeficiente() << std::endl;
+        // std::cout << "Grado" << i << ": " << monomio.GetGrado() << std:: endl;
+    }
+
+    for(i=0;i < coeff.size();i++){
+        std::cout << "Coef " << i << ": " << coeff[i] << std::endl;
+    }
+
+    // for(i=0; i<3;i++){
+    //     Monomio monomio = monomios[i];
+    //     double aux = monomio.GetGrado();
+    //     coeff.push_back(aux);
+
+    //     std::cout << "Coef " << i << ": " << coeff[i] << std::endl;
+    // }
+
+    /*
     for (i = 0; i < monomios.size(); i++) {
       Monomio monomio = monomios[i];
       std::cout << monomio.GetCoeficiente() << "x^" << monomio.GetGrado()
                 << " ";
-    }
+    }*/
     std::cout << std::endl;
 
     double resultado = Utils::evaluar(monomios, valor);
@@ -43,3 +65,15 @@ int main(int argc, char **argv) {
   }
   return exitCode;
 }
+
+// int vectorCoef(vector<Monomio> monomios, vector<int> coeff){
+//     int i;
+//     for(i=0; i<coeff.size();i++){
+//         Monomio monomio = monomios[i];
+//         int aux = monomio.GetGrado();
+        
+//         int grado = coeff.push_back(aux);
+//         cout << "Coef " << i << ": " << grado << endl;
+//     }
+
+// }
